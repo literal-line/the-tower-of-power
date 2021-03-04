57d733e45477f453986ceb26da56caa00382b20f
Literal Line <chickenfishbird2@gmail.com>
2021-03-04 15:37:43 +0000

    fix service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
-var version = 'v0.1-20210304-1030est';
+var version = 'v0.1-20210304-1037est';
 var assets = [
   './',
-  './index.html'
+  './index.html',
+  './the-tower-of-power.js',
+  './style.css',
+  './assets/floors.json',
+  './assets/5-seconds-of-silence.mp3',
+  './assets/arcade_n.ttf',
+  './assets/iconHelp.png',
+  './assets/iconControls.png',
+  './assets/font.png',
+  './assets/quiquePixel.png',
+  './assets/title.png',
+  './assets/tilesFloor.png',
+  './assets/jingle.mp3',
+  './assets/insertCredit.mp3',
+  './assets/roundStart.mp3',
+  './assets/roundPlay.mp3'
 ];
 
 self.addEventListener('install', function (e) {
